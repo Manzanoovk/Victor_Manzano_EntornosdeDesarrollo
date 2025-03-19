12544a4fb4218097bc449b57fe69673ed101d2cc
Manzanoovk <vmanzanobartual@gmail.com>
2025-03-19 20:14:18 +0100

    Guardar cambios

---
+++ VMB_Circulo.java
@@ -0,0 +1,41 @@
+public class VMB_Circulo {
+    private int id;
+    private double radio;
+    private String color;
+
+    public VMB_Circulo() {
+    }
+
+    public VMB_Circulo(String color, int id, double radio) {
+        this.color = color;
+        this.id = id;
+        this.radio = radio;
+    }
+
+    public double getRadio() {
+        return radio;
+    }
+
+    public void setRadio(double radio) {
+        this.radio = radio;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("VMB_Circulo{");
+        sb.append("id=").append(id);
+        sb.append(", radio=").append(radio);
+        sb.append(", color=").append(color);
+        sb.append('}');
+        return sb.toString();
+    }
+}
